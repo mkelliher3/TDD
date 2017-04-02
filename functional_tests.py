b389e5da555d538f381044e0ed7fffaaea5080f8
“mkelliher3” <m.kelliher3@gmail.com>
2017-04-02 15:21:56 +0100

    Three strikes and refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -13,34 +13,34 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # on homepage
         self.browser.get('http://localhost:8000')
 
-        # notices the page title and header mention to-do lists
+        # assert title and header
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # invited to enter a to-do item straight away
+        # first test on inputbox
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-
-        # she types 'buy peacock feathers' into a text box
         inputbox.send_keys('Buy peacock feathers')
-
-        # hit Enter key and then the page is updated and should show the new item
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        # second test on inputbox.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # asserting table
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         self.fail('Finish the test!')
 
