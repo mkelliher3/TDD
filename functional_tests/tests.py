66b077eb4f4579e2c5f2f962cd475352b5c8c239
“mkelliher3” <m.kelliher3@gmail.com>
2017-04-06 01:43:18 +0100

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,12 @@
 import time
-import unittest
 
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
 
 MAX_WAIT = 10
+
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -17,24 +17,18 @@ class NewVisitorTest(LiveServerTestCase):
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
-        # here is the loop that will go forever, unless we get to one of two possible exit routes
         while True:
             try:
                 table = self.browser.find_element_by_id('id_list_table')
                 rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
-                return # if the assertions passes, we return from the function and escape the loop
-            # if we catch an exception, we wait a short amount of time and loop around to retry.
-            # WebDriverException - when the page is not loaded or table element is not there.
-            # AssertionError - when the table is there, so it doesn't have rows in yet.
+                return
             except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT: # This is the second escape route.
+                if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
 
-    # def test_can_start_a_list_and_retrieve_it_later(self):
     def test_can_start_a_list_for_one_user(self):
-        # self.browser.get('http://localhost:8000')
         self.browser.get(self.live_server_url)
 
         # assert title and header
@@ -63,12 +57,8 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.fail('Finish the test!')
 
-    # def test_can_start_a_list_for_one_user(self):
-    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
-    #     self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        # Edith starts a new todo list
+        # Edith starts a new to do list
         self.browser.get(self.live_server_url)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -80,9 +70,6 @@ class NewVisitorTest(LiveServerTestCase):
         # she notices that her list has a unique URL
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
-        # assertRegex is a helper function from unittest that checks whether
-        # a string matches a regular experession. We use it to check that
-        # our new REST-ish design has been implemented.
 
         # a new user, Francis, comes along to the site
 
@@ -112,4 +99,3 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-
